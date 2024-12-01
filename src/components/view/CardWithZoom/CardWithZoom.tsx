c4dd93b9078d4051653a2e313563c2d4beafec00
Forhad Khan <87197002+xnafi@users.noreply.github.com>
2024-12-01 13:50:16 +0600

    code modified

--- src/components/view/CardWithZoom/CardWithZoom.tsx
+++ src/components/view/CardWithZoom/CardWithZoom.tsx
@@ -8,16 +8,10 @@ const CardWithZoom = ({
 }) => {
   return (
     <div className="relative w-64 h-80 overflow-hidden rounded-lg cursor-pointer group">
-      <div className="absolute inset-0 transition-transform duration-500 ease-out transform group-hover:scale-110">
-        <Image
-          src={imageSrc}
-          alt={name}
-          layout="fill"
-          objectFit="cover"
-          className="rounded-lg"
-        />
+      <div className="absolute inset-0 transition-transform duration-500 ease-out transform group-hover:scale-125">
+        <Image src={imageSrc} alt={name} fill className="rounded-lg" />
       </div>
-      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40">
+      <div className="absolute inset-0 flex items-center justify-center text-center bg-black bg-opacity-40">
         <p className="text-white text-2xl font-semibold">{name}</p>
       </div>
     </div>
