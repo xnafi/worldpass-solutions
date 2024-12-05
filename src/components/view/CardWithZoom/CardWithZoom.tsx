2b089cf0daf6075cc195b873d33056f09057a121
Forhad Khan <87197002+xnafi@users.noreply.github.com>
2024-12-05 16:14:20 +0600

    played with gsap

--- src/components/view/CardWithZoom/CardWithZoom.tsx
+++ src/components/view/CardWithZoom/CardWithZoom.tsx
@@ -8,7 +8,7 @@ const CardWithZoom = ({
 }) => {
   return (
     <div className="relative w-64 h-80 overflow-hidden rounded-lg cursor-pointer group">
-      <div className="absolute inset-0 transition-transform duration-500 ease-out transform group-hover:scale-125">
+      <div className="absolute inset-0 transition-transform duration-500 ease-out transform group-hover:scale-125" id="#fred">
         <Image src={imageSrc} alt={name} fill className="rounded-lg" />
       </div>
       <div className="absolute inset-0 flex items-center justify-center text-center bg-black bg-opacity-40">
--- src/utils/FadeOut.tsx
+++ src/utils/FadeOut.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { ReactNode, useRef, useEffect } from "react";
+import React, { ReactNode, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -12,32 +13,59 @@ interface FadeOutProps {
 
 const FadeOut: React.FC<FadeOutProps> = ({ children }) => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const childRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (containerRef.current) {
-      gsap.fromTo(
-        containerRef.current.children,
-        {
-          opacity: 1,
-          y: 0,
-        },
-        {
-          opacity: 0,
-          y: -50,
-          duration: 1,
-          stagger: 0.2,
-          scrollTrigger: {
-            trigger: containerRef.current,
-            start: "40% 20%", // Animation starts when top reaches 80% of viewport
-            end: "40% 20%", // Animation ends when top reaches 20% of viewport
-            scrub: true,
-          },
-        }
-      );
-    }
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "10% 80%",
+        end: "10% 65%",
+        markers: true,
+        scrub: true,
+      },
+    });
+
+    tl.to(childRef.current, {
+      opacity: "1",
+      yPercent: "100",
+      stagger: 0.2,
+      duration: 0.8,
+      ease: "elastic.inOut",
+    });
+    tl.from(childRef.current, {
+      yPercent: "100",
+      opacity: "0.1",
+    });
+
+    // if (containerRef.current) {
+    //   gsap.fromTo(
+    //     containerRef.current.children,
+    //     {
+    //       opacity: 1,
+    //       y: 0,
+    //     },
+    //     {
+    //       opacity: 0,
+    //       y: -50,
+    //       duration: 1,
+    //       stagger: 0.2,
+    //       scrollTrigger: {
+    //         trigger: containerRef.current,
+    //         start: "40% 20%", // Animation starts when top reaches 80% of viewport
+    //         end: "40% 20%", // Animation ends when top reaches 20% of viewport
+    //         scrub: true,
+    //       },
+    //     }
+    //   );
+    // }
   }, []);
 
-  return <div ref={containerRef}>{children}</div>;
+  return (
+    <div ref={containerRef}>
+      <div ref={childRef}>{children}</div>
+    </div>
+  );
 };
 
 export default FadeOut;
